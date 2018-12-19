3cf3f5fa2a46895c27cc5a84cb07b6fed27ebd27
Александр Сайфутдинов <tykataf@yandex.ru>
2018-12-20 00:36:36 +0500

    js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,37 @@
 var popup = document.querySelector(".modal-write");
 var butn = document.querySelector(".map-btn");
-var closemap = popup.querySelector(".close-btn");
+var closewrite = popup.querySelector(".close-btn");
+var popupmap = document.querySelector(".modal-map");
+var closemap = popupmap.querySelector(".close-btn-map");
+var mapup = document.querySelector(".map-link");
+var buybtn = itemactive.querySelectorAll(".buy-btn");
+var popupadd = document.querySelector(".modal-add");
+var closeadd = popupadd.querySelector(".close-btn");
+var cataloglist = document.querySelector(".catalog-list");
+var listitem = cataloglist.querySelector(".list-item");
+var itemactive = listitem.querySelector(".item-active");
 
 butn.addEventListener("click", function (evt) {
 evt.preventDefault();
 popup.classList.add("modal-show");
 });
 
-closemap.addEventListener("click", function (evt) {
+closewrite.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.remove("modal-show");
+});
+
+mapup.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popupmap.classList.add("modal-show");
+});
+
+closemap.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popupmap.classList.remove("modal-show");
+});
+
+buybtn.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popupadd.classList.add("modal-show");
 });
