1a613a47c6685e25f362073cb7b7e902b06f3e8c
Александр Сайфутдинов <tykataf@yandex.ru>
2018-12-19 21:07:43 +0500

    Работа над ошибками
    исправление модального окна

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-var popup = document.querySelector(".modal-add");
-var butn = document.querySelectorAll(".buy-btn");
+var popup = document.querySelector(".modal-write");
+var butn = document.querySelector(".map-btn");
+var closemap = popup.querySelector(".close-btn");
 
 butn.addEventListener("click", function (evt) {
 evt.preventDefault();
 popup.classList.add("modal-show");
+});
+
+closemap.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.remove("modal-show");
 });
